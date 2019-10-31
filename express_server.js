dd5de5d92181da7df629a653ac1f63486e40ac67
Fred <thecanuckatheist@gmail.com>
2019-10-31 00:27:08 +0000

    fixed the delete and edit at server

--- express_server.js
+++ express_server.js
@@ -8,8 +8,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "aJ48lW"},
+  "9sm5xK": { longURL: "http://www.google.com", userID: "aJ48lW"}
 };
 
 const users = { 
@@ -55,6 +55,21 @@ const checkPass = function(str, pass, users){
   return "";
 };
 
+const urlsForUser = function(id, urlData){  // checks if userID matches inside urlDatabase and returns
+  let arr = [];                             // array of short urls
+  for (let item in urlData) {
+    let obj = urlData[item];
+    console.log(typeof id); // checking type
+    console.log(typeof obj['userID']);  //checking type
+    console.log(id);
+    console.log(obj['userID']);
+    if (obj['userID'] == id) {
+      arr.push(item);
+    }
+  }
+  return arr;
+};
+
 let templateVars = {};
 
 app.set("view engine", "ejs");
@@ -64,13 +79,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  templateVars = { urls: urlDatabase, user_id: req.cookies["user_id"] };
+  templateVars = { urls: urlDatabase, user_id: req.cookies['user_id'] };
   res.render("urls_register", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  templateVars = { urls: urlDatabase, user_id: req.cookies["user_id"] };
-
+  templateVars = { urls: urlDatabase, user_id: req.cookies['user_id'] };
   res.render("urls_index", templateVars);
 });
 
@@ -79,21 +93,26 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],  user_id: req.cookies["user_id"]};
+  let obj = urlDatabase[req.params.shortURL];
+  urlDatabase[req.params.shortURL] = {longURL: obj['longURL'], userID: req.cookies['user_id']};
+  templateVars = { urls: urlDatabase, shortURL: req.params.shortURL, longURL: obj['longURL'],  user_id: req.cookies['user_id']};
   res.render("urls_show", templateVars);
 });
 
 app.get("/login", (req, res) => {
-  templateVars = { urls: urlDatabase, user_id: req.cookies["user_id"] };
+  templateVars = { urls: urlDatabase, user_id: req.cookies['user_id'] };
   res.render("urls_login", templateVars);
 });
 
-app.post("/urls", (req,res) => {
-  urlDatabase[generateRandomString()] = req.body['longURL'];
+app.post("/urls", (req,res) => { // main page
+  let urlObj = {};
+  urlObj['longURL'] = req.body['longURL'];
+  urlObj['userID'] = req.cookies['user_id'];
+  urlDatabase[generateRandomString()] = urlObj;
   res.redirect("/urls");
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get("/u/:shortURL", (req, res) => {  // sends us to the longURL
   // const longURL = ...s
   const shortUrl = req.params.shortURL;
 
@@ -101,18 +120,32 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {  // edit -> to render urls_show
-  templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user_id: req.cookies["user_id"]};
+  let obj = urlDatabase[req.params.shortURL]; 
+  templateVars = { shortURL: req.params.shortURL, longURL: obj['longURL'], user_id: req.cookies['user_id']};
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {  // delete
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
+  let newArr = urlsForUser(req.cookies['user_id'], urlDatabase);  //creates the arr
+  if (newArr.length === 0) {   // if arr is empty, then the user is not logged in or has nothing stored in
+    res.redirect("/login");  //redirect to log in
+  } else {
+    if (newArr.indexOf(req.params.shortURL) > -1) {  // checks if the item to delete is inside the arr
+      delete urlDatabase[req.params.shortURL];  // deletes if its in
+    }
+    res.redirect("/urls");  // redirects back to main page
+  }
 });
 
-app.post("/urls/:id", (req, res) => {  // delete
-  urlDatabase[req.params.id] = req.body['longURL'];
-  res.redirect("/urls");
+app.post("/urls/:id", (req, res) => {  // edit
+  let newArr = urlsForUser(req.cookies['user_id'], urlDatabase);  //creates the arr
+  if (newArr.length === 0) {   // if arr is empty, then the user is not logged in or has nothing stored in
+    res.redirect("/login");  //redirect to log in
+  } else {
+    let obj = urlDatabase[req.params.id];   // updates the database with new longURL
+    urlDatabase[req.params.id] = {longURL: obj['longURL'], userID: req.cookies['user_id']};
+    res.redirect("/urls");
+  }
 });
 
 app.post("/login", (req, res) => {
